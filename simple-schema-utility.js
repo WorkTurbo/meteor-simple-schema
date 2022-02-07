6f05daca5727b596c2e2df785935e88e9262c3e0
gregory <greg2502@gmail.com>
2022-02-07 17:36:56 +0100

    make Object more lenient

--- simple-schema-utility.js
+++ simple-schema-utility.js
@@ -5,7 +5,7 @@ Utility = {
     if (key === "$each") {
       return affectedKey;
     } else {
-      return (affectedKey ? affectedKey + "." + key : key);
+      return affectedKey ? affectedKey + "." + key : key;
     }
   },
   shouldCheck: function shouldCheck(key) {
@@ -15,19 +15,21 @@ Utility = {
     return !_.contains(["$pull", "$pullAll", "$pop", "$slice"], key);
   },
   errorObject: function errorObject(errorType, keyName, keyValue) {
-    return {name: keyName, type: errorType, value: keyValue};
+    return { name: keyName, type: errorType, value: keyValue };
   },
-  // Tests whether it's an Object as opposed to something that inherits from Object
+  // Tests whether it's an Object
   isBasicObject: function isBasicObject(obj) {
-    return _.isObject(obj) && Object.getPrototypeOf(obj) === Object.prototype;
+    return _.isObject(obj);
   },
   // The latest Safari returns false for Uint8Array, etc. instanceof Function
   // unlike other browsers.
   safariBugFix: function safariBugFix(type) {
-    return (typeof Uint8Array !== "undefined" && type === Uint8Array) ||
+    return (
+      (typeof Uint8Array !== "undefined" && type === Uint8Array) ||
       (typeof Uint16Array !== "undefined" && type === Uint16Array) ||
       (typeof Uint32Array !== "undefined" && type === Uint32Array) ||
-      (typeof Uint8ClampedArray !== "undefined" && type === Uint8ClampedArray);
+      (typeof Uint8ClampedArray !== "undefined" && type === Uint8ClampedArray)
+    );
   },
   isNotNullOrUndefined: function isNotNullOrUndefined(val) {
     return val !== void 0 && val !== null;
@@ -35,7 +37,9 @@ Utility = {
   // Extracts operator piece, if present, from position string
   extractOp: function extractOp(position) {
     var firstPositionPiece = position.slice(0, position.indexOf("["));
-    return (firstPositionPiece.substring(0, 1) === "$") ? firstPositionPiece : null;
+    return firstPositionPiece.substring(0, 1) === "$"
+      ? firstPositionPiece
+      : null;
   },
   deleteIfPresent: function deleteIfPresent(obj, key) {
     if (key in obj) {
@@ -51,7 +55,7 @@ Utility = {
     return false;
   },
   dateToDateString: function dateToDateString(date) {
-    var m = (date.getUTCMonth() + 1);
+    var m = date.getUTCMonth() + 1;
     if (m < 10) {
       m = "0" + m;
     }
@@ -59,6 +63,6 @@ Utility = {
     if (d < 10) {
       d = "0" + d;
     }
-    return date.getUTCFullYear() + '-' + m + '-' + d;
-  }
+    return date.getUTCFullYear() + "-" + m + "-" + d;
+  },
 };
